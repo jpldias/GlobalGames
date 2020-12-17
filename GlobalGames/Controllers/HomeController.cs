7b89126131c1616ea17762181d676d23e5718570
Joao Dias <Joao.Lourenco.Dias@formandos.cinel.pt>
2020-12-17 16:04:14 +0000

    implementação do form dos serviços

--- GlobalGames/Controllers/HomeController.cs
+++ GlobalGames/Controllers/HomeController.cs
@@ -1,11 +1,38 @@
-﻿using GlobalGames.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GlobalGames.Data;
+using GlobalGames.Data.Entidades;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Threading.Tasks;
+using GlobalGames.Data;
+using GlobalGames.Data.Entidades;
+using GlobalGames.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+
 
 namespace GlobalGames.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly DataContext _context;
+
+        public HomeController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+
         public IActionResult Index()
         {
             return View();
@@ -23,7 +50,21 @@ namespace GlobalGames.Controllers
             return View();
         }
 
-       
+        // POST: Contactos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Servicos([Bind("Id,Name,Email,msg")] Contactos contactos)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(contactos);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
